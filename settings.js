d534b89fc8d9352d42f406872ba2760b6b4fb35a
JeroMbuh <137664653+JeroMbuh@users.noreply.github.com>
2023-06-30 13:18:20 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 global.creator = 'Dange_Menggila'// yourname
 global.MONGO_DB_URI = "mongodb+srv://geronomika2:<altarim123#>@cluster0.mb2jzyz.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "geronomika2@gmail.com" //email
-global.email_password = "altarim123#" //application password email
+global.your_email = "geronomika123@gmail.com" //email
+global.email_password = "lnicdazlafpncpji" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
